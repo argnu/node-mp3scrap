cc03a1fbb3feb6332593b656a060f2b189d39c62
Martin Weingart <martinweingart@gmail.com>
2017-06-01 23:16:26 -0300

    - Script en python para escanear carpetas (más rápido que node).

--- db/models/folder.js
+++ db/models/folder.js
@@ -6,7 +6,8 @@ module.exports = function (sequelize, DataTypes) {
     path: { type: Sequelize.STRING, field: 'path' },
     search_art: { type: Sequelize.BOOLEAN, field: 'search_art' },
     scanned: { type: Sequelize.BOOLEAN, field: 'scanned' },
-    last_scan: { type: Sequelize.DATE, field: 'last_scan' }
+    last_scan: { type: Sequelize.DATE, field: 'last_scan' },
+    scan_finished: { type: Sequelize.BOOLEAN, field: 'scan_finished' }
   }, {
     freezeTableName: true,
     indexes: [{unique: true, fields: ['path']}],
